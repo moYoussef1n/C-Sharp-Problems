f6ced317ca41c1595f2d278055529443dceaedf4
Muhammad Youssef <muhammadyoussef1n@gmail.com>
2026-06-23 16:43:35 +0300

    Solves SumUpChallenge

---
+++ Easy/SumUpChallenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SumUpChallenge;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var sum = 0;
+
+        while (true)
+        {
+
+
+            System.Console.WriteLine(@"Please enter a number or ""ok"" to exit. ");
+            var input = Console.ReadLine();
+
+            if (input == "ok")
+            {
+                break;
+            }
+            
+            int number = Convert.ToInt32(input);
+            
+            sum += number;
+        }
+
+        System.Console.WriteLine($"Total sum = {sum}");
+    }
+}
