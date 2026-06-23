00086255c1c3801b521e4e74f39df22fa0ed1c6c
Muhammad Youssef <muhammadyoussef1n@gmail.com>
2026-06-23 16:25:12 +0300

    Solves divisible by 3 challenge with step tracing

---
+++ Easy/DivisibleByThreeChallenge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DivisibleByThreeChallenge;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var count = 0; 
+        for (int i = 1 ; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                count += 1 ;
+                System.Console.WriteLine($"Count {count} is: {i}");
+            }
+        }
+        System.Console.WriteLine($"Total counts is {count}");
+    }
+}
