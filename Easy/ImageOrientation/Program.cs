e60e2e0f89131efef0a5cbebdd966169ecd0fdd0
Muhammad Youssef <muhammadyoussef1n@gmail.com>
2026-06-23 08:33:50 +0300

    Easy: Add ImageOrientation solution

---
+++ Easy/ImageOrientation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ImageOrientation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Please enter the width");
+
+        var width = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine("Please enter the height");
+
+        var height = Convert.ToInt32(Console.ReadLine());
+
+        if (width > height)
+        {
+            System.Console.WriteLine("The image is landscape.");
+        }
+        else
+        {
+            System.Console.WriteLine("The image is portrait.");
+        }
+
+
+
+    }
+}
