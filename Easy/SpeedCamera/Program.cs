ebc699119a156f22771809ecbde2cb70d84a4fdd
Muhammad Youssef <muhammadyoussef1n@gmail.com>
2026-06-23 08:56:56 +0300

    Easy: Add SpeedCamera demerit points solution

---
+++ Easy/SpeedCamera/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SpeedCamera;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int AllowedSpeed = 90;
+
+        var finePoints = 0;
+
+        var speedDifference = 0;
+
+        System.Console.WriteLine("Please enter car speed");
+
+        var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+        if (carSpeed <= 90)
+        {
+            System.Console.WriteLine("Ok!");
+        }
+        else
+        {
+            speedDifference = carSpeed - AllowedSpeed;
+            finePoints = speedDifference / 5 ;
+
+            if (finePoints > 12 )
+            {
+                System.Console.WriteLine("License Suspended");
+            }
+            else
+            {
+                System.Console.WriteLine($"Care! You have {finePoints} fine points");
+            }
+        }
+
+
+    }
+}
