1ec02dc7e7895f7a62d6b87303eb00725510ff73
Muhammad Youssef <muhammadyoussef1n@gmail.com>
2026-06-23 16:54:09 +0300

    Compute Factorial

---
+++ Easy/ComputeFactorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ComputeFactorial;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Please enter your fav number");
+        var number = Convert.ToInt32(Console.ReadLine());
+
+        long factorial = 1 ;
+
+        for (int i = number ; i >= 1; i--)
+        {
+            factorial *= i ;
+        }
+
+        System.Console.WriteLine($"Facorial of number {number} = {factorial}");
+    }
+}
