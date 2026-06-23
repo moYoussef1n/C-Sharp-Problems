94aaafc3141d4ce820bd7473ad9ac4a5f6f96528
Muhammad Youssef <muhammadyoussef1n@gmail.com>
2026-06-23 08:15:46 +0300

    Add MaxNumber solution

---
+++ Easy/MaxNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MaxNumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Please enter your first number");
+
+        var firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine("Please enter your second number");
+
+        var secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        if (firstNumber > secondNumber)
+        {
+            Console.WriteLine($"Max Number: {firstNumber}");
+        }
+        else
+        {
+            Console.WriteLine($"Max Number: {secondNumber}");
+        }
+
+
+
+    }
+}
