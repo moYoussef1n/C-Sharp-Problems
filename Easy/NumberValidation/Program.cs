05ef4797533a1861b9bee42e3ce232e68b1245a9
Muhammad Youssef <muhammadyoussef1n@gmail.com>
2026-06-23 07:54:15 +0300

    Add NumberValidation solution in Easy folder

---
+++ Easy/NumberValidation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NumberValidation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Please enter number in range 1 : 10");
+
+        var inputNumber = Convert.ToInt32(Console.ReadLine());
+
+        if (inputNumber >= 1 && inputNumber <= 10)
+        {
+            System.Console.WriteLine("Valid");
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid");
+        }
+    }
+}
