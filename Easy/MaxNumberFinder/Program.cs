74d856a9a3b98f33909ea5047da945cd4eb7a613
Muhammad Youssef <muhammadyoussef1n@gmail.com>
2026-06-24 13:13:14 +0300

    Find the max number

---
+++ Easy/MaxNumberFinder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MaxNumberFinder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Please enter seperated comma numbers");
+
+        var input = Console.ReadLine();
+
+        var numbers = input.Split(',');
+
+        var max = Convert.ToInt32(numbers[0]);
+
+        foreach (var str in numbers)
+        {
+           var number = Convert.ToInt32(str);
+            if ( number > max)
+            {
+                max = number;
+            }
+        }
+        System.Console.WriteLine($"Max found number is {max}");
+    }
+}
